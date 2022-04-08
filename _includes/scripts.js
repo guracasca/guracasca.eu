8b2c47c8a39d0d9e210669bdabaff94245b403f0
Dumitru C <canteadumitru@gmail.com>
2022-04-08 21:49:28 +0300

    Update scripts.js

--- _includes/scripts.js
+++ _includes/scripts.js
@@ -59,10 +59,10 @@ function launchSearchBox() {
         <div class="section mx-5">
           <div class="card">
             <div class="panel">
-              <div class="panel-heading">Search</div>
+              <div class="panel-heading">Cauta</div>
               <div class="panel-block">
                 <p class="control has-icons-left">
-                  <input class="input" id="input-search" type="text" placeholder="search..." onkeyup="search()">
+                  <input class="input" id="input-search" type="text" placeholder="cauta..." onkeyup="search()">
                   <span class="icon is-left">
                   <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" aria-hidden="true" role="img" style="vertical-align: -0.125em;" width="18" height="18" preserveAspectRatio="xMidYMid meet" viewBox="0 0 36 36"><path class="clr-i-outline clr-i-outline-path-1" d="M16.33 5.05A10.95 10.95 0 1 1 5.39 16A11 11 0 0 1 16.33 5.05m0-2.05a13 13 0 1 0 13 13a13 13 0 0 0-13-13z" fill="#4a4a4a"/><path class="clr-i-outline clr-i-outline-path-2" d="M35 33.29l-7.37-7.42l-1.42 1.41l7.37 7.42A1 1 0 1 0 35 33.29z" fill="#4a4a4a"/></svg>
                   </span>
